9b54f42680dca69812c036274e1a76be35ee1ebf
Eugene Zorin <ezorin5@yandex.ru>
2023-02-17 21:56:35 +0400

    fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+require('dotenv-vault-core').config();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
